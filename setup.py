20ea0266dc89f816c73db5d8768d9d060da217ba
Insun Ahn <ais1406@gmail.com>
2012-11-12 20:09:16 +0900

    Use py2app

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(
 	name="wxchat",
 	version ="1.0",
 	py_modules=["wxchat","chatnetworking","chatserverStub","rendezvous"],
 	data_files = [
     	('images', ['images/conn.jpg', 'images/exit.jpg', 'images/info.jpg'])
-	]
+	],
+	app=['wxchat.py'],
+	options = {'py2app': {'argv_emulation': True}},
+	setup_requires = ['py2app'],
 )
